b3bbc21f91936467f08dd9c44de4dd4e606a1c37
Lucca-Xavier <luccacxavier@gmail.com>
2024-11-05 14:42:32 -0300

    update funcionando no postman

--- server/routes/produtosRoutes.js
+++ server/routes/produtosRoutes.js
@@ -1,73 +1,121 @@
-const express = require('express');
-const Product = require('../models/Product'); 
+const express = require("express");
+const Product = require("../models/Product");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const produtos = await Product.findAll(); 
-        res.status(200).json(produtos);
-    } catch (error) {
-        console.error('Erro ao buscar produtos:', error); 
-        res.status(500).json({ error: 'Erro ao buscar produtos' });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const produtos = await Product.findAll();
+    res.status(200).json(produtos);
+  } catch (error) {
+    console.error("Erro ao buscar produtos:", error);
+    res.status(500).json({ error: "Erro ao buscar produtos" });
+  }
 });
 
+router.post("/add", async (req, res) => {
+  const { name, author, category, sinopse, year, image, price, qntEstoque } =
+    req.body;
 
+  if (
+    !name ||
+    !author ||
+    !category ||
+    !sinopse ||
+    !year ||
+    !price ||
+    !qntEstoque
+  ) {
+    return res.status(422).json({ error: "Preencha todos os campos" });
+  }
 
-router.post('/add', async (req, res) => {
-    const { name, author, category, sinopse, year, image, price, qntEstoque } = req.body;
+  try {
+    const productExists = await Product.findOne({ where: { name } });
 
-    if (!name || !author || !category || !sinopse || !year || !price || !qntEstoque) {
-        return res.status(422).json({ error: 'Preencha todos os campos' });
+    if (productExists) {
+      const quantidadeAtualizada = productExists.qntEstoque + qntEstoque;
+      await Product.update(
+        { qntEstoque: quantidadeAtualizada },
+        { where: { id: productExists.id } }
+      );
+
+      return res
+        .status(200)
+        .json({ message: "Quantidade atualizada no estoque" });
+    } else {
+      const product = new Product({
+        name,
+        author,
+        category,
+        sinopse,
+        year,
+        image,
+        price,
+        qntEstoque,
+      });
+
+      await product.save();
+      return res
+        .status(201)
+        .json({ message: "Produto adicionado com sucesso", product });
     }
-
-    try {
-        const productExists = await Product.findOne({ where: { name } }); 
-
-        if (productExists) {
-            const quantidadeAtualizada = productExists.qntEstoque + qntEstoque;
-            await Product.update(
-                { qntEstoque: quantidadeAtualizada },
-                { where: { id: productExists.id } }
-            );
-
-            return res.status(200).json({ message: "Quantidade atualizada no estoque" });
-        } else {
-            const product = new Product({
-                name,
-                author,
-                category,
-                sinopse,
-                year,
-                image,
-                price,
-                qntEstoque
-            });
-
-            await product.save();
-            return res.status(201).json({ message: 'Produto adicionado com sucesso', product });
-        }
-    } catch (error) {
-        console.error(error); 
-        res.status(500).json({ error: 'Erro ao adicionar produto' });
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao adicionar produto" });
+  }
 });
-
 
 router.delete("/:id/delete", async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params;
 
-        const produtos = await Product.destroy({where: {id}})
+    const produtos = await Product.destroy({ where: { id } });
 
-        if(!produtos){
-            return res.status(404).json({ error: "Produto não encontrado"})
-        }
-
-        res.status(200).json({message: "Produto deletado"})
-    } catch (error) {
-        console.log("Erro ao deletar produto")        
+    if (!produtos) {
+      return res.status(404).json({ error: "Produto não encontrado" });
     }
-})
+
+    res.status(200).json({ message: "Produto deletado" });
+  } catch (error) {
+    console.log("Erro ao deletar produto");
+  }
+});
+
+router.put("/:id/update", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, author, category, sinopse, year, image, price, qntEstoque } =
+      req.body;
+
+      const produto = await Product.findByPk(id)
+      if(!produto){
+        return res.status(404).json({error: "Produto não encontrado"})
+      }
+
+    if (
+      !name ||
+      !author ||
+      !category ||
+      !sinopse ||
+      !year ||
+      !image ||
+      !price ||
+      !qntEstoque
+    ) {
+      return res.status(422).json({ error: "Preencha todos os campos" });
+    }
+
+    await Product.update(
+        { name,  author, category, sinopse, year, image, price, qntEstoque },
+        { where: { id } }
+    )
+
+    res.status(200).json({message: "Produto editado"})
+    console.log(message)
+
+
+  } catch (error) {
+    console.log("Erro ao editar produto")
+  }
+});
 
 module.exports = router;
